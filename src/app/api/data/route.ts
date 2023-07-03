af2d96c3d0721b5c27cde28e33d40f486af9838a
Tanyawat Vittaypalaotai <hamzaabamboo@gmail.com>
2023-07-03 17:10:35 +0700

    feat: remove unused page

--- src/app/api/data/route.ts
+++
@@ -1,7 +0,0 @@
-import { readdir } from "fs/promises";
-import { join } from "path";
-
-export async function GET(request: Request) {
-    const files = await readdir(join("../../../data"))
-    return files
-}
